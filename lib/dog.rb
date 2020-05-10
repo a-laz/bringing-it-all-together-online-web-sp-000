452a6f0821b0e9b7620d41389c077fad7c14234b
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-05-10 06:45:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,6 +23,7 @@ class Dog
   def save
     DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?,?)", self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    binding.pry
     self
   end
 
